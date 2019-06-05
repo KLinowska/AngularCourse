b8be8cdf80dcfa613eee86b5ebc348a3eff378d6
klinowska <linowska.k.a@gmail.com>
2019-06-05 09:29:39 +0200

    Encapsulate ProductListComponent style, build custom pipe for converting character to space, add filter products functionality

--- angular-getting-started/src/app/app.module.ts
+++ angular-getting-started/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
+import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ConvertToSpacesPipe,
     ProductListComponent
   ],
   imports: [
