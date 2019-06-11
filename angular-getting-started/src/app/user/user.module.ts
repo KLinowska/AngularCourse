a6aa5336529db647c00d8591bcb37e6e5c536037
klinowska <linowska.k.a@gmail.com>
2019-06-11 12:41:31 +0200

    Add routing for log in and log out

--- angular-getting-started/src/app/user/user.module.ts
+++ angular-getting-started/src/app/user/user.module.ts
@@ -3,9 +3,13 @@ import { NgModule } from '@angular/core';
 import { LoginComponent } from './login.component';
 
 import { SharedModule } from '../shared/shared.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
+    RouterModule.forChild([
+      { path: 'login', component: LoginComponent }
+    ]),
     SharedModule
   ],
   declarations: [
